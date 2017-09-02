063be3a69814b036361bfea7bcafa5fa1ce057ab
Justin Spies <justin.spies@buyerquest.com>
2017-09-02 17:01:49 -0400

    Simplify script since it does not need to provide compatibility with Pipelines; they are handled entirely by the bitbucket-pipeline.yml file

--- validate-cookbook.sh
+++ validate-cookbook.sh
@@ -1,78 +1,37 @@
 #!/bin/bash
 
 
-ls -latr ~/.ssh
-
 # Setup the SSH private key for Git clone operations
-printf '%s\n' "$PRIVKEY" > ~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa
-
-printf "$CR_SOURCE" | grep 'http' > /dev/null
-if [ $? -ne 0 ] ; then
-  # The source URL is not HTTP, assume SSH and grab the host/port to add the key to the known_hosts
-  SOURCE_HOST=$(printf "$CR_SOURCE" | sed 's;\(\(ssh\)://\)*\(.*@\)*\([A-Za-z_\.0-9-]*\):*\([A-Za-z0-9]*\)*/\(.*\);\4;g')
-  SOURCE_PORT=$(printf "$CR_SOURCE" | sed 's;\(\(ssh\)://\)*\(.*@\)*\([A-Za-z_\.0-9-]*\):*\([A-Za-z0-9]*\)*/\(.*\);-p \5;g')
-  if [ "$SOURCE_PORT" == "-p " ] ; then
-    ssh-keyscan -T60 $SOURCE_HOST >> ~/.ssh/known_hosts
-  else
-    ssh-keyscan -T60 $SOURCE_PORT $SOURCE_HOST >> ~/.ssh/known_hosts
-  fi
+if [ ! -z "$PRIVKEY" ] ; then
+  printf 'Adding private key from $PRIVKEY\n'
+  printf '%s\n' "$PRIVKEY" > ~/.ssh/id_rsa
+  chmod 0600 ~/.ssh/id_rsa
 fi
 
-if [ -z $BUILD_DIR ] ; then
-  printf "$CB_SOURCE" | grep 'http' > /dev/null
-  if [ $? -ne 0 ] ; then
-    # The source URL is not HTTP, assume SSH and grab the host/port to add the key to the known_hosts
-    SOURCE_HOST=$(printf "$CB_SOURCE" | sed 's;\(\(ssh\)://\)*\(.*@\)*\([A-Za-z_\.0-9-]*\):*\([A-Za-z0-9]*\)*/\(.*\);\4;g')
-    SOURCE_PORT=$(printf "$CB_SOURCE" | sed 's;\(\(ssh\)://\)*\(.*@\)*\([A-Za-z_\.0-9-]*\):*\([A-Za-z0-9]*\)*/\(.*\);-p \5;g')
-    if [ "$SOURCE_PORT" == "-p " ] ; then
-      ssh-keyscan -T60 $SOURCE_HOST >> ~/.ssh/known_hosts
-    else
-      ssh-keyscan -T60 $SOURCE_PORT $SOURCE_HOST >> ~/.ssh/known_hosts
-    fi
-  fi
+if [ ! -z "$SSH_HOSTS" ] ; then
+  for ok_host in $(printf "$SSH_HOSTS") ; do 
+    printf "Host $ok_host\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+  done
 fi
 
+# Set a default if the environment value is not specified
 cd /chef
-BR='master'
-if [ ! -z $CR_BRANCH ] ; then
-  BR="$CR_BRANCH"
-fi
+BR="${CR_BRANCH:-master}"
+printf 'Checking out the chef-repo code from %s@%s\n' "$CR_SOURCE" "$BR"
 git archive --remote="${CR_SOURCE}" "$BR" | tar -xf -
 
-if [ -z $BUILD_DIR ] ; then
-  cd /chef/cookbook
-  BR='master'
-  if [ ! -z $CB_BRANCH ] ; then
-    BR="$CB_BRANCH"
-  fi
-  git archive --remote="${CB_SOURCE}" "$BR" | tar -xf -
-else
-  rm -rf /chef/cookbook
-  ln -s $BUILD_DIR /chef/cookbook 
-fi
+cd /chef/cookbook
+BR="${CB_BRANCH:-master}"
+printf 'Checking out the cookbook code from %s@%s\n' "$CB_SOURCE" "$BR"
+git archive --remote="${CB_SOURCE}" "$BR" | tar -xf -
 
-printf 'Running RuboCop...'
-rubocop
-COP_RET=$?
-printf 'return value is %s\n' "$COP_RET"
-if [ $COP_RET -ne 0 ] ; then
-  exit $COP_RET
-fi
+printf 'Running RuboCop...' ; rubocop ; COP_RET=$?
+printf 'Running FoodCritic...' ; foodcritic . ; FC_RET=$?
+printf 'Running ChefSpec...' ; chef exec rspec ; CS_RET=$?
 
-printf 'Running FoodCritic...'
-foodcritic .
-printf 'return value is %s\n' "$FC_RET"
-FC_RET=$?
-if [ $FC_RET -ne 0 ] ; then
-  exit $FC_RET
-fi
-
-printf 'Running ChefSpec...'
-chef exec rspec
-CS_RET=$?
-printf 'return value is %s\n' "$CS_RET"
-if [ $CS_RET -ne 0 ] ; then
-  exit $CS_RET
+if [[ $CP_RET -ne 0 || $FC_RET -ne 0 || $CS_RET -ne 0 ]] ; then
+  printf 'Detected one or more failures, returning non-zero result\n'
+  printf 'RuboCop = %s; FoodCritic = %s ; ChefSpec = %s\n' "$COP_RET" "$FC_RET" "$CS_RET"
+  exit 1
 fi
 
